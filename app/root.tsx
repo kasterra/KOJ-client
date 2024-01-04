d78e43babaca01714d857f85a4f8c96805c6454c
kasterra <kasterra@naver.com>
2024-01-04 11:25:09 +0900

    feat(styles) : add fonts, global and normalize css

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,8 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
+import normailzeCSS from "./css/normalize.css";
+import fontsCSS from "./css/fonts.css";
+import globalCSS from "./css/global.css";
 import {
   Links,
   LiveReload,
@@ -10,12 +13,15 @@ import {
 } from "@remix-run/react";
 
 export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: normailzeCSS },
+  { rel: "stylesheet", href: fontsCSS },
+  { rel: "stylesheet", href: globalCSS },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="ko">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
