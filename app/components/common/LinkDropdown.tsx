80503e58b3d9e621254fd663ed47bbd1fdd783e6
kasterra <kasterra@naver.com>
2024-01-19 21:17:02 +0900

    feat : LinkDropdown optional onClickList 추가

--- app/components/common/LinkDropdown.tsx
+++ app/components/common/LinkDropdown.tsx
@@ -4,13 +4,18 @@ import styles from "./dropdown.module.css";
 interface Props {
   submenuTitles: string[];
   submenuLinks: string[];
+  onClickList?: (() => void)[];
 }
 
-const LinkDropdown = ({ submenuTitles, submenuLinks }: Props) => {
+const LinkDropdown = ({ submenuTitles, submenuLinks, onClickList }: Props) => {
   return (
     <div className={styles["dropdown-item-container"]}>
       {submenuLinks.map((submenuLink, index) => (
-        <Link to={submenuLink} key={submenuLink}>
+        <Link
+          to={submenuLink}
+          key={submenuLink}
+          onClick={onClickList ? onClickList[index] : undefined}
+        >
           <div className={styles["dropdown-item"]}>{submenuTitles[index]}</div>
         </Link>
       ))}
