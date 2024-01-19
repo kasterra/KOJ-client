cb9646c07d9c41845b995caa8935f4af903a399a
kasterra <kasterra@naver.com>
2024-01-19 16:09:14 +0900

    feat : TextArea component

---
+++ app/components/Input/TextArea.tsx
@@ -0,0 +1,45 @@
+import styles from "./input.module.css";
+
+interface Props {
+  title: string;
+  name: string;
+  placeholder?: string;
+  description?: string;
+  required?: boolean;
+  defaultValue?: string;
+  width?: number;
+  height?: number;
+}
+
+const TextArea = ({
+  title,
+  name,
+  placeholder,
+  description,
+  required,
+  defaultValue,
+  width,
+  height,
+}: Props) => {
+  return (
+    <div className={styles.wrapper}>
+      <label className={styles.title} htmlFor={name}>
+        {title}
+      </label>
+      <textarea
+        className={styles.input}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        required={required}
+        defaultValue={defaultValue}
+        style={width && height ? { width, height } : undefined}
+      />
+      {description?.length && (
+        <span className={styles.description}>{description}</span>
+      )}
+    </div>
+  );
+};
+
+export default TextArea;
