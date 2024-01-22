3aa294a2a57822f932d11e400645b3270e0dc2c4
kasterra <kasterra@naver.com>
2024-01-22 20:38:55 +0900

    feat : /students page auth feature

--- app/routes/_procted+/students+/$lectureId/index.tsx
+++ app/routes/_procted+/students+/$lectureId/index.tsx
@@ -1,7 +1,19 @@
+import { useAuth } from "~/contexts/AuthContext";
 import Table from "./Table";
 import styles from "./index.module.css";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "@remix-run/react";
 
 const Wrapper = () => {
+  const auth = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (auth.role !== "professor") {
+      toast.error("교수 전용 페이지입니다. 교수 계정으로 로그인 하세요");
+      navigate("/");
+    }
+  }, []);
   return (
     <div className={styles.container}>
       <Table />
