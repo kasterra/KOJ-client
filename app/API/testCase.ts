111117398eed828e35ef43d4aa1605f9f44b5d8b
kasterra <kasterra@naver.com>
2024-05-19 23:51:32 +0900

    feat(testCase) : delete file API

--- app/API/testCase.ts
+++ app/API/testCase.ts
@@ -140,10 +140,23 @@ export async function deleteTestcase(testcaseId: number, token: string) {
 
 export async function deleteFileInputFromTestCase(
   testcaseId: number,
-  token: string,
-  fileName: string
+  fileName: string,
+  token: string
 ) {
-  const response = { status: 404 };
+  const response = await fetch(
+    `${API_SERVER_URL}/testcase/${testcaseId}/file`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        kind: "input",
+        name: fileName,
+      }),
+    }
+  );
   switch (response.status) {
     case 401:
       handle401();
@@ -151,7 +164,7 @@ export async function deleteFileInputFromTestCase(
     case 403:
       throw new Error("강의 소유 권한이 없습니다. 다시 확인해 주세요");
     case 404:
-      throw new Error("현재 구현되어 있지 않은 API 입니다");
+      throw new Error("존재하지 않는 TC ID 입니다");
     case 500:
       throw new Error("서버 에러가 발생했습니다. 관리자에게 문의해 주세요");
   }
@@ -159,10 +172,23 @@ export async function deleteFileInputFromTestCase(
 
 export async function deleteFileOutputFromTestCase(
   testcaseId: number,
-  token: string,
-  fileName: string
+  fileName: string,
+  token: string
 ) {
-  const response = { status: 404 };
+  const response = await fetch(
+    `${API_SERVER_URL}/testcase/${testcaseId}/file`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        kind: "output",
+        name: fileName,
+      }),
+    }
+  );
   switch (response.status) {
     case 401:
       handle401();
@@ -170,7 +196,7 @@ export async function deleteFileOutputFromTestCase(
     case 403:
       throw new Error("강의 소유 권한이 없습니다. 다시 확인해 주세요");
     case 404:
-      throw new Error("현재 구현되어 있지 않은 API 입니다");
+      throw new Error("존재하지 않는 TC ID 입니다");
     case 500:
       throw new Error("서버 에러가 발생했습니다. 관리자에게 문의해 주세요");
   }
